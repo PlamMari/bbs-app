9ba5c86cdb2704c181deddfb9aaeb04770d637bd
PlamMari <plammaris@gmail.com>
2024-02-25 17:40:02 +0200

    added provider

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/*" element={<App />} />
-      </Routes>      
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path="/*" element={<App />} />
+        </Routes>      
+      </Router>
+    </Provider>    
   </React.StrictMode>,
   document.getElementById('root')
 );
