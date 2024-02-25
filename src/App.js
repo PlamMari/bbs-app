6d75eb5169c43e823decef96b66655992a9030b5
PlamMari <plammaris@gmail.com>
2024-02-25 18:39:55 +0200

    counter finished

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { format } from 'date-fns';
 import Layout from './Layout';
+import Counter from './features/counter/Counter';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -68,29 +69,29 @@ function App() {
   }
 
   return ( 
-    <Routes>
-      <Route path="/" element={<Layout 
+    <><Routes>
+      <Route path="/" element={<Layout
         search={search}
-        setSearch = {setSearch}          
-        />}>
-        <Route index element={<Home posts={searchResults} />} />      
+        setSearch={setSearch} />}>
+        <Route index element={<Home posts={searchResults} />} />
         <Route path="post">
           <Route index element={<NewPost
             handleSubmit={handleSubmit}
             postTitle={postTitle}
             setPostTitle={setPostTitle}
             postBody={postBody}
-            setPostBody={setPostBody}
-        />} />
-          <Route path=":id" element={<PostPage 
-          posts={posts}
-          handleDelete={handleDelete}
-          />} />
+            setPostBody={setPostBody} />} />
+          <Route path=":id" element={<PostPage
+            posts={posts}
+            handleDelete={handleDelete} />} />
         </Route>
         <Route path="about" element={<About />} />
         <Route path="*" element={<Missing />} />
       </Route>
-    </Routes>   
+    </Routes>
+    <main className="App">
+        <Counter />
+      </main></>
   );
 }
 
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -7,15 +7,21 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
+  reset
 } from './counterSlice';
 import styles from './Counter.module.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
   const dispatch = useDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState('0');
 
   const incrementValue = Number(incrementAmount) || 0;
+
+  const resetAll = () => {
+    setIncrementAmount(0);
+    dispatch(reset());
+  } 
 
   return (
     <div>
@@ -65,3 +71,5 @@ export function Counter() {
     </div>
   );
 }
+
+export default Counter
